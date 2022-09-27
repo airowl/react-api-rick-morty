50d06ce207b783e0d2f3b3e71030871d76cf19f4
rowl <airowl.gasga@gmail.com>
2022-09-27 21:07:14 +0200

    header section

--- src/header.js
+++ src/header.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './header.scss';
 
 const Header = () => {
   return (
-    <header className=''>
-      Header
+    <header>
+      <div className='container h-100 d-flex align-items-center justify-content-center'>
+            <h1>Rick and Morty</h1>
+      </div>
     </header>
   )
 }
